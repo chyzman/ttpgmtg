023d4aa706b8c5a04ff760972031dc41af977a56
Jakob <minenash@protonmail.com>
2024-06-18 20:45:08 -0400

    Added card-pixel based child adding

--- src/card_template.tsx
+++ src/card_template.tsx
@@ -1,35 +1,58 @@
-import { Color, GameObject, Player, UIElement, refObject, Vector, Text, Canvas, Rotator, HorizontalBox, Button, LayoutBox } from "@tabletop-playground/api";
+import {
+    Color,
+    GameObject,
+    UIElement,
+    refObject,
+    Vector,
+    Text,
+    Canvas,
+    Rotator,
+    Border, Widget, TextJustification
+} from "@tabletop-playground/api";
 
 const SCRYFALL = "https://api.scryfall.com/";
+const IMG_WIDTH = 672;
+const IMG_HEIGHT = 936;
+const SCALE = 1/16;
+
+const RED: Color = new Color(1,0,0,1);
+const BLUE: Color = new Color(0,0,1,1);
+
+let uiWidthMult = 0;
+let uiHeightMult = 0;
 
 ((obj: GameObject) => {
     const size = obj.getSize();
-
     const ui = new UIElement();
-
+    
+    ui.scale = SCALE;
+    ui.position = new Vector(0, 0, -0.05);
     ui.rotation = new Rotator(180, 180, 0);
-    ui.position = new Vector(size.x/2,size.y/2,-size.z / 2 - 0.01);
     ui.useWidgetSize = false;
-    ui.width = size.x/0.05;
-    ui.height = size.y/0.05;
-    ui.scale = 0.05;
-
+    ui.width = obj.getSize().y * 10 / SCALE;
+    ui.height = obj.getSize().x * 10 / SCALE;
+    uiWidthMult = ui.width / IMG_WIDTH;
+    uiHeightMult = ui.height / IMG_HEIGHT;
+    
     let canvas = new Canvas();
+    let label = new Text().setText("Test").setTextColor(RED).setFontSize(12);
+    
+    imgPositionedChild(canvas, new Text().setText(uiWidthMult.toString()), 0, 0, IMG_WIDTH, IMG_HEIGHT);
+    imgPositionedChild(canvas, new Text().setText(uiHeightMult.toString()), 0, 10, IMG_WIDTH, IMG_HEIGHT);
+    
+    testImgPositionedChild(RED,canvas, new Text().setText(".").setJustification(TextJustification.Right), 538, 848, 38, 29);
+    testImgPositionedChild(BLUE, canvas, new Text().setText("/").setJustification(TextJustification.Center), 576, 848, 13, 29);
+    testImgPositionedChild(RED, canvas, new Text().setText(".").setJustification(TextJustification.Left), 589, 848, 38, 29);
+    
+    
     ui.widget = canvas;
-
-    let powerToughnessHolder = new HorizontalBox();
-
-    let powerDisplay = new Button().setText("13").setFontSize(100);
-    let powerToughnessDivider = new Text().setText("/").setFontSize(100);
-    let toughnessDisplay = new Button().setText("27").setFontSize(100); 
-    
-    powerToughnessHolder.addChild(powerDisplay).addChild(powerToughnessDivider).addChild(toughnessDisplay);
-
-    let ptLayout = new LayoutBox();
-    ptLayout.setChild(powerToughnessHolder);
-
-    canvas.addChild(ptLayout, 0, 0, 100, 200)
-    
     obj.addUI(ui);
 
-})(refObject);  
+})(refObject);
+    
+function imgPositionedChild(canvas: Canvas, widget: Widget, x: number, y: number, width: number, height: number) {
+    return canvas.addChild(widget, x * uiWidthMult, y * uiHeightMult, width * uiWidthMult, height * uiHeightMult);
+}
+function testImgPositionedChild(color: Color, canvas: Canvas, widget: Widget | undefined, x: number, y: number, width: number, height: number) {
+    return imgPositionedChild(canvas, new Border().setColor(color).setChild(widget), x, y, width, height);
+}

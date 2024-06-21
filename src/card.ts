a0d43a884b9ec5871f44e19b9bc257e383909c3b
chyzman <32403637+chyzman@users.noreply.github.com>
2024-06-20 21:14:57 -0400

    mmmm yummi segfault

--- src/card.ts
+++ src/card.ts
@@ -14,13 +14,10 @@ import {
 } from "@tabletop-playground/api";
 import {loadMtg} from "./Loader/chyzMtg";
 
-const SCRYFALL = "https://api.scryfall.com/";
 const IMG_WIDTH = 672;
 const IMG_HEIGHT = 936;
 const SCALE = 1 / 16;
 
-const RED: Color = new Color(1, 0, 0, 1);
-const BLUE: Color = new Color(0, 0, 1, 1);
 const BLACK: Color = new Color(0, 0, 0, 1);
 
 let uiWidthMult = 0;
@@ -32,19 +29,13 @@ let uiHeightMult = 0;
     let toughness = 3;
     let tapped = false;
 
-    const ui = new UIElement();
+    const ui = createUIElement();
+    const hoverUI = createUIElement(UIZoomVisibility.Regular);
+    const zoomedUI = createUIElement(UIZoomVisibility.ZoomedOnly);
 
-    ui.scale = SCALE;
-    ui.position = new Vector(0, 0, -0.05);
-    ui.rotation = new Rotator(180, 180, 0);
-    ui.useWidgetSize = false;
-    ui.width = obj.getSize().y * 10 / SCALE;
-    ui.height = obj.getSize().x * 10 / SCALE;
-    ui.zoomVisibility = UIZoomVisibility.Both;
-    uiWidthMult = ui.width / IMG_WIDTH;
-    uiHeightMult = ui.height / IMG_HEIGHT;
-
-    let canvas = new Canvas();
+    const canvas = new Canvas();
+    const hoverCanvas = new Canvas();
+    const zoomedCanvas = new Canvas();
 
     let font = 48;
 
@@ -63,42 +54,72 @@ let uiHeightMult = 0;
 
     let toughnessDisplay = testImgPositionedChild(BLACK, canvas, new Text().setText("3").setFontSize(font).setJustification(TextJustification.Center), startX + numWidth + slashWidth, startY, numWidth, height);
 
-    let powerIncreaser = imgPositionedChild(canvas, new Button().setText("+"), startX, startY - modifierHeight, numWidth, modifierHeight);
+    let powerIncreaser = imgPositionedChild(hoverCanvas, new Button().setText("+"), startX, startY - modifierHeight, numWidth, modifierHeight);
     powerIncreaser.onClicked.add(() => power++);
 
-    let powerDecreaser = imgPositionedChild(canvas, new Button().setText("-"), startX, startY + height, numWidth, modifierHeight);
+    let powerDecreaser = imgPositionedChild(hoverCanvas, new Button().setText("-"), startX, startY + height, numWidth, modifierHeight);
     powerDecreaser.onClicked.add(() => power--);
 
-    let toughnessIncreaser = imgPositionedChild(canvas, new Button().setText("+"), startX + numWidth + slashWidth, startY - modifierHeight, numWidth, modifierHeight);
+    let toughnessIncreaser = imgPositionedChild(hoverCanvas, new Button().setText("+"), startX + numWidth + slashWidth, startY - modifierHeight, numWidth, modifierHeight);
     toughnessIncreaser.onClicked.add(() => toughness++);
 
-    let toughnessDecreaser = imgPositionedChild(canvas, new Button().setText("-"), startX + numWidth + slashWidth, startY + height, numWidth, modifierHeight);
+    let toughnessDecreaser = imgPositionedChild(hoverCanvas, new Button().setText("-"), startX + numWidth + slashWidth, startY + height, numWidth, modifierHeight);
     toughnessDecreaser.onClicked.add(() => toughness--);
 
 
     ui.widget = canvas;
+    hoverUI.widget = hoverCanvas;
+    zoomedUI.widget = zoomedCanvas;
     obj.addUI(ui);
+    obj.addUI(hoverUI);
+    obj.addUI(zoomedUI);
 
     obj.onTick.add((o) => {
+        powerDisplay.setText(power.toString());
+        toughnessDisplay.setText(toughness.toString());
+    });
+
+    const interval = setInterval(() => {
         const permission = new PlayerPermission();
         world.getAllPlayers().forEach(player => {
-            const pos = o.worldPositionToLocal(player.getCursorPosition())
-            const scale = o.getScale()
-            const extent = o.getExtent(false, true)
+            const pos = obj.worldPositionToLocal(player.getCursorPosition())
+            const scale = obj.getScale()
+            const extent = obj.getExtent(false, true)
             extent.x /= scale.x
             extent.y /= scale.y
             const isInside = pos.x >= -extent.x && pos.x <= extent.x && pos.y >= -extent.y && pos.y <= extent.y
             if (isInside || player.getHighlightedObject() == obj) permission.addPlayer(player);
         });
-        if (ui.players.value != permission.value) {
-            ui.players = permission;
-            o.updateUI(ui);
+        if (permission.value === 0) {
+            obj.removeUIElement(hoverUI)
+        } else if (!obj.getUIs().includes(hoverUI)) {
+            obj.addUI(hoverUI)
         }
+        if (hoverUI.players.value != permission.value) {
+            hoverUI.players = permission;
+            obj.updateUI(hoverUI);
+        }
+    }, 50);
+    obj.onDestroyed.add(() => clearInterval(interval));
 
-        powerDisplay.setText(power.toString());
-        toughnessDisplay.setText(toughness.toString());
-    });
+    function createUIElement(zoomVisibility: number = UIZoomVisibility.Both) {
+        const ui = new UIElement();
+        const getPosition = () => new Vector(0, 0, -(obj.getSize().z / 2 - 0.01));
+        ui.scale = SCALE;
+        ui.position = getPosition();
+        ui.rotation = new Rotator(180, 180, 0);
+        ui.useWidgetSize = false;
+        ui.width = obj.getSize().y * 10 / SCALE;
+        ui.height = obj.getSize().x * 10 / SCALE;
+        ui.zoomVisibility = zoomVisibility;
+        uiWidthMult = ui.width / IMG_WIDTH;
+        uiHeightMult = ui.height / IMG_HEIGHT;
+        const interval = setInterval(() => ui.position = getPosition());
+        obj.onDestroyed.add(() => clearInterval(interval));
+        return ui;
+    }
 })(refObject);
+
 
 function imgPositionedChild<T extends Widget>(canvas: Canvas, widget: T, x: number, y: number, width: number, height: number) {
     canvas.addChild(widget, x * uiWidthMult, y * uiHeightMult, width * uiWidthMult, height * uiHeightMult);
@@ -108,8 +129,4 @@ function imgPositionedChild<T extends Widget>(canvas: Canvas, widget: T, x: numb
 function testImgPositionedChild<T extends Widget>(color: Color, canvas: Canvas, widget: T, x: number, y: number, width: number, height: number) {
     imgPositionedChild(canvas, new Border().setColor(color).setChild(widget), x, y, width, height);
     return widget;
-}
-
-const makeCanvas = (obj: GameObject) => {
-
 }
